3ff7020dc524e7dee857d079d1b66cd5ef5e89ad
Chris Tate <chris_tate_94@hotmail.com>
2022-01-10 21:35:20 +0000

    added string template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,17 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(
-      `🟢🟢🟢 Assertion Passed: [actual output] === [expected output]`
-    );
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
   }
   if (actual !== expected) {
-    console.log(
-      `🔴🔴🔴 Assertion Failed: [actual output] !== [expected output]`
-    );
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
