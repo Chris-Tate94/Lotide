b38eb0de5705cbd23401d200687e3c604327a163
Chris Tate <chris_tate_94@hotmail.com>
2022-01-12 21:41:36 +0000

    added function to get middle value(s) from an array

--- middle.js
+++ middle.js
@@ -25,3 +25,27 @@ const assertArraysEqual = function (array1, array2) {
   }
   return console.log(`🟢🟢🟢 Assertion Passed: [${array1}] === [${array2}]`);
 };
+
+const middle = function (array) {
+  //return an empty array if array is empty or has less than 2 elements
+  let result = [];
+  if (array.length <= 2) {
+    return result;
+  }
+  //if array is an odd length
+  if (array.length % 2 !== 0) {
+    result = Math.ceil(array.length / 2);
+  }
+  // if array is even length,
+  if (array.length % 2 === 0) {
+    let n = array.length;
+    //get the value at the second middle index
+    let a2 = array[n / 2];
+    //get the value at the first middle index
+    let a1 = array[n / 2 - 1];
+    result = [a1, a2];
+  }
+  return result;
+};
+//console.log(middle([]));
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
