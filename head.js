302f2fd5d8404417da92e973312a428316b057db
Chris Tate <chris_tate_94@hotmail.com>
2022-01-17 19:31:54 +0000

    refactor

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-  }
-  if (actual !== expected) {
-    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 //Head function to return back first element in an array
 
@@ -13,7 +6,4 @@ const head = function (array) {
   return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]));
+module.exports = head;
