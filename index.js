1468c9d0b13f19dee1a8085920b8c6320a703b5c
Chris Tate <chris_tate_94@hotmail.com>
2022-01-17 23:04:34 +0000

    added all functions to export

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const head = require("./head");
 const tail = require("./tail");
 const middle = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKeys = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const letterPosition = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKeyByValue: findKeyByValue,
+  findKeys: findKeys,
+  letterPosition: letterPosition,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
 };
