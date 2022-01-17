8fcb065ae902ac37c4c6b69ed42b7c82543de2a0
Chris Tate <chris_tate_94@hotmail.com>
2022-01-17 20:56:40 +0000

    added mocha and chai assertion testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,11 @@
-const assertArraysEqual = require("../assertArraysEqual");
+//const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+const assert = require("chai").assert;
+
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.strictEqual(middle([1, 2, 3, 4, 5]), 3);
+  });
+});
+
 module.exports = middle;
