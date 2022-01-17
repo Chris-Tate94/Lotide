d96581aaa004efb076d123564f06ca3363e16f41
Chris Tate <chris_tate_94@hotmail.com>
2022-01-17 21:11:55 +0000

    refactor

--- middle.js
+++ middle.js
@@ -23,4 +23,7 @@ const middle = function (array) {
 };
 //console.log(middle([]));
 
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+
 module.exports = middle;
--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
-//   }
-//   if (actual !== expected) {
-//     console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
-
 const assertEqual = require("./assertEqual");
 
 //Tail function to return all elements of an array excluding the first
